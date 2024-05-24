e825ab7a1c87303735ca287175f6cc299a76daf9
ramyus889 <ramyus889.com>
2024-05-24 20:54:00 +0300

    first

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_GATEWAY_URL:
-      "https://green-imaginative-trout-795.mypinata.cloud",
-    NEXT_PUBLIC_BASE_URL: "https://api.pinata.cloud",
-  },
   reactStrictMode: true,
   async redirects() {
     return [
